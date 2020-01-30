e01a81535660f98490621f080e9612d325703bd9
merfanian <erfanianmahdi00@gmail.com>
2020-01-30 12:16:12 +0330

    Implement Login page

---
+++ src/Login.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
+
+import { withRouter } from 'react-router-dom';
+
+class Login extends Component {
+    constuctor() {
+        this.routeChange = this.routeChange.bind(this);
+        this.state = { role: 'guest' };
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const form = {
+            role: this.state.role,
+        };
+        if (
+            form.role == 'admin' ||
+            form.role == 'patient' ||
+            form.role == 'doctor'
+        )
+            window.location.href = '/' + form.role;
+        else alert('not permitted');
+    };
+
+    handleChange = e => {
+        this.setState({
+            role: e.target.value,
+        });
+    };
+
+    render() {
+        return (
+            <Dialog
+                open
+                onRequestClose={this.props.toggleLogin}
+                fullScreen={this.props.fullScreen}
+            >
+                <DialogTitle>Login</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="username"
+                        label="Username"
+                        type="username"
+                        onChange={e => this.handleChange(e)}
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={e => this.onSubmit(e)} color="primary">
+                        Login
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        );
+    }
+}
+
+export default withMobileDialog()(Login);
