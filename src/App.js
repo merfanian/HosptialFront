9aa6ac6c1a4a3a6533cd8755ecf6d17ccf301439
merfanian <erfanianmahdi00@gmail.com>
2020-01-30 12:48:17 +0330

    Add other pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Login from './Login';
+import Main from './Main';
 function App() {
     return (
         <div className="App">
             <header className="App-header"></header>
-            <Login></Login>
+            <Main></Main>
         </div>
     );
 }
---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from '../src/Login';
+import Home from '../src/Login';
+import Admin from '../src/Login';
+import Patient from '../src/Login';
+import Doctor from '../src/Login';
+
+const Main = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/admin" component={Admin}></Route>
+            <Route exact path="/patient" component={Patient}></Route>
+            <Route exact path="/doctor" component={Doctor}></Route>
+        </Switch>
+    );
+};
+
+export default Main;
