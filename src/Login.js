f96acfe7e2155d8dd9a10a04f6ebfc33f247935c
merfanian <erfanianmahdi00@gmail.com>
2020-02-03 00:49:33 +0330

    Change login page

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
+import Radio from '@material-ui/core/Radio';
 
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -13,20 +14,28 @@ import { withRouter } from 'react-router-dom';
 
 class Login extends Component {
     constuctor() {
-        this.routeChange = this.routeChange.bind(this);
-        this.state = { role: 'guest' };
+        this.state = { id: '', role: '' };
     }
 
     onSubmit = e => {
         e.preventDefault();
         const form = {
-            role: this.state.role,
+            id: this.state.id,
         };
-
-        window.location.href = '/' + 'patient' + '?' + 'id=' + form.role;
+        if (this.state.role == '' || this.state.role == null)
+            window.location.href = '/' + 'patient' + '?' + 'id=' + form.id;
+        else
+            window.location.href =
+                '/' + 'doctor' + '?' + 'id=' + this.state.role;
     };
 
     handleChange = e => {
+        this.setState({
+            id: e.target.value,
+        });
+    };
+
+    handleDoctorChange = e => {
         this.setState({
             role: e.target.value,
         });
@@ -49,6 +58,17 @@ class Login extends Component {
                             label="Username"
                             type="username"
                             onChange={e => this.handleChange(e)}
+                            fullWidth
+                        />
+                    </DialogContent>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="doctorname"
+                            label="EmpId"
+                            type="username"
+                            onChange={e => this.handleDoctorChange(e)}
                             fullWidth
                         />
                     </DialogContent>
